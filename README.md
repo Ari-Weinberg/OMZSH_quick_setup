6ec87cd08599ae8b8e36b9da1f32aa12976fadbc
Ari-Weinberg <67128976+Ari-Weinberg@users.noreply.github.com>
2022-08-02 11:58:05 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # OMZSH_quick_setup
 Quick setup of oh my zsh with essential plugins
+
+This is a 1 liner to install zsh, oh my zsh, install some essential plugins, and change the theme.
+Run with:
+```
+
+```
---
+++ omzsh-setup.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt install zsh -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+zsh -c "git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
+zsh -c "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="bira"/g' ~/.zshrc
+sudo chsh -s $(which zsh) $(whoami)
+zsh
